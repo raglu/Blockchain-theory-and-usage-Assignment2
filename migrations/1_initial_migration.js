3a50810e9d3e7302799e3703663028823fef9899
raglu <raglu16@student.sdu.dk>
2021-10-21 13:03:17 +0200

    MetaCoin copy paste
    and rename to Bank

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 const Account = artifacts.require("Account");
+const Bank = artifacts.require("Bank");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Account);
+  deployer.deploy(Bank);
 };
